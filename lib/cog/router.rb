ce7d8d15a1e0c418bd8f4f0e06c58fedeacc4a6b
Mark Imbriaco <mark@imbriaco.com>
2017-01-19 13:28:38 -0500

    infer class names when absent from routes

--- lib/cog/router.rb
+++ lib/cog/router.rb
@@ -12,23 +12,33 @@ module Cog
       if route = @routes[command_name]
         command = route[:class].new(request)
         command.send(route[:method])
+        command.response.write unless command.response.nil?
       else
         abort("No route found for command #{command_name}.")
       end
     end
 
     def self.route_for(name, action)
-      if action.nil?
-        {
-          :class => Object.const_get(name.gsub("-", "_").camelize),
-          :method => :run_command
-        }
+      case action
+      when nil
+        { :class => class_for(name.gsub("-", "_").camelize),
+          :method => :run_command }
       else
-        klass, method = action.split("#")
-        {
-          :class => Object.const_get(klass),
-          :method => method&.to_sym || :run_command
-        }
+        class_name, method = action.split("#")
+        { :class => class_for(class_name),
+          :method => method&.to_sym || :run_command }
+      end
+    end
+
+    private
+
+    def self.class_for(name)
+      klass = Object.const_get(name)
+
+      if klass.ancestors.include?(Cog::Command)
+        klass
+      else
+        raise(RuntimeError, "Error: #{name} must be a subclass of Cog::Command")
       end
     end
   end
